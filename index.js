30c7402a967b7e3cfa117e0fb500fd9209e4f15a
Artur <info@aigroupusa.com>
2021-12-28 16:53:55 -0500

    commit tree adc8bea2e74ac8f52aaaca7d6ea78add98355866

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+function map(sourceArray, fn) {
+    return sourceArray.map(fn);
+}
+
+
+function reduce(sourceArray, fn, startingPoint) {
+    const keys = Object.keys(sourceArray)
+    let i = 1
+
+    if (startingPoint) {
+        i = 0
+    } else {
+        startingPoint = sourceArray[keys[0]]
+    }
+
+    for (i; i < keys.length; i++)  {
+        startingPoint = fn(startingPoint, sourceArray[keys[i]]);
+    }
+    return startingPoint
+ }
